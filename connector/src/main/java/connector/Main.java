bebc220652f5c86ee4f52d65705a0ccd37ff0a11
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2022-03-22 01:28:23 +0530

    Add add, remove, delete Index logic

--- connector/src/main/java/connector/Main.java
+++ connector/src/main/java/connector/Main.java
@@ -43,9 +43,95 @@ public class Main {
             stmt.executeUpdate(sql);
             System.out.println("Created table in given database...");
 
+            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
+            stmt.executeUpdate(sql);
+
+
+            sql = "Insert into Registration(first, last, age) values ('first37', 'last37', 23);";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration(first, last, age) values ('first48', 'last48', 23);";
+            stmt.executeUpdate(sql);
+
+
+            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
+            stmt.executeUpdate(sql);
+
+
             sql = "CREATE hashbit Index secondary_index on REGISTRATION(first);";
             stmt.executeUpdate(sql);
             System.out.println("Created index in given database...");
+
+            sql = "CREATE hashbit Index secondary_index_last on REGISTRATION(last);";
+            stmt.executeUpdate(sql);
+            System.out.println("Created index in given database...");
+
+            System.out.println("?????????????????????????????  This is Part 02 ????????????????????????????????????????????????????????");
+
+            sql = "Insert into Registration(first, last, age) values ('first37', 'last37', 23);";
+            stmt.executeUpdate(sql);
+
+
+            sql = "Insert into Registration(first, last, age) values ('first48', 'last48', 23);";
+            stmt.executeUpdate(sql);
+
+
+            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration() values ();";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration() values ();";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration(age) values (31);";
+            stmt.executeUpdate(sql);
+
+            sql = "Insert into Registration(first) values ('first13');";
+            stmt.executeUpdate(sql);
+
+//            null => 10, 11, 12
+
+            System.out.println("-----------------------------------------------------UPDATE---------------------------------------------------------------------");
+
+            sql = "UPDATE Registration SET  first = 'update6', last = 'update6' WHERE ID = 6;";
+            stmt.executeUpdate(sql);
+
+            sql = "UPDATE Registration SET  first = 'update8', last = 'update8' WHERE ID = 8;";
+            stmt.executeUpdate(sql);
+
+            sql = "UPDATE Registration SET  first = 'update1', last = 'update1' WHERE ID = 1;";
+            stmt.executeUpdate(sql);
+
+            sql = "UPDATE Registration SET  first = 'update3', last = 'update3' WHERE ID = 3;";
+            stmt.executeUpdate(sql);
+
+            sql = "UPDATE Registration SET  first = 'first13', last = 'last13' WHERE ID = 10;";
+            stmt.executeUpdate(sql);
+
+            sql = "UPDATE Registration SET  first = NULL, last = NULL WHERE ID = 11;";
+            stmt.executeUpdate(sql);
+
+            System.out.println("----------------------------------------------------DELETE----------------------------------------------------------------------");
+//
+//
+            sql = "DELETE FROM Registration WHERE id = 3;";
+            stmt.executeUpdate(sql);
+
+            sql = "DELETE FROM Registration WHERE id = 10;";
+            stmt.executeUpdate(sql);
+
+            sql = "DELETE FROM Registration WHERE id = 1;";
+            stmt.executeUpdate(sql);
+//
+            System.out.println("-----------------------------------------------------------SELECT-------------------------------------------------------");
 
             // STEP 4: Clean-up environment
             stmt.close();
@@ -70,3 +156,38 @@ public class Main {
         } //end try
     }
 }
+
+//first26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//first37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//first48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 ]
+//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0 ]
+//first13 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
+//first159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
+
+//update 6 8 1 3
+
+//first26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//first37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
+//first48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 ]
+//first13 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
+//update8 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ]
+//update6 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ]
+//first159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 ]
+
+
+//last26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//last37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//last48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 ]
+//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 ]
+//last159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
+
+//update 6 8 1 3
+
+//last26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ]
+//last37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//last48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ]
+//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0 , 0 ]
+//update3 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
+//last159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+//update1 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 ]
