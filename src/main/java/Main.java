b416c831d6d7c644c0dc4b0bdb90c11a76bd899a
Thumula Perera <thumusteam@gmail.com>
2022-03-26 16:14:27 +0530

    Add auto insert test rows

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import com.github.javafaker.Faker;
 import org.h2.command.dml.Delete;
 
 import java.sql.*;
+import lombok.extern.slf4j.Slf4j;
+import org.h2.table.Column;
+import org.h2.value.TypeInfo;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+@Slf4j
 public class Main {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "org.h2.Driver";
@@ -13,165 +20,92 @@ public class Main {
     static final String USER = "sa";
     static final String PASS = "";
 
+    // faker for mock data
+    private static final Faker faker = new Faker();
+
+    private static final String TABLE_NAME = "demo";
+    private static final Column PRIMARY_KEY = new Column("id", TypeInfo.TYPE_BIGINT);
+    private static final List<Column> OTHER_COLUMNS = Arrays.asList(
+            new Column("fname", TypeInfo.TYPE_VARCHAR),
+            new Column("lname", TypeInfo.TYPE_VARCHAR)
+    );
+
+    private static final String FNAME_HASHBIT_INDEX_NAME = "FNAME_HASHBIT_INDEX";
+    private static final String LNAME_HASHBIT_INDEX_NAME = "LNAME_HASHBIT_INDEX";
+
+    private static final int FNAME_HASHBIT_INDEX_BUCKETS = 16;
+
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
         try {
-            String sql; ResultSet resultSet;
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
+            setUpDbConnection();
+            initStmt();
 
-            //STEP 2: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            // drop able if exists
+            executeSQL(SqlHelper.getDropTableSql(TABLE_NAME));
+
+            // create table
+            executeSQL(SqlHelper.getCreateTableSql(
+                    TABLE_NAME,
+                    PRIMARY_KEY,
+                    OTHER_COLUMNS
+            ));
+
+            // create hashbit index
+            executeSQL(SqlHelper.getCreateHashIndexSql(
+                    FNAME_HASHBIT_INDEX_NAME,
+                    TABLE_NAME,
+                    OTHER_COLUMNS.get(0).getName(),
+                    FNAME_HASHBIT_INDEX_BUCKETS
+            ));
+
+            // insert 1000 rows
+            for (int i = 0; i < 1000; i++) {
+                executeSQL(SqlHelper.getInsertSql(
+                        TABLE_NAME,
+                        OTHER_COLUMNS,
+                        Arrays.asList(
+                            faker.name().firstName(),
+                            faker.name().lastName()
+                        )
+                ));
+            }
 
 
-            System.out.println("Drop table in given database...");
-            stmt = conn.createStatement();
-            sql =  "DROP TABLE REGISTRATION IF EXISTS;";
-            stmt.executeUpdate(sql);
 
-            sql = "CREATE TABLE REGISTRATION (id bigint auto_increment, first VARCHAR(255),last VARCHAR(255), age INTEGER, PRIMARY KEY (id));";
-            stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
-
-            sql = "CREATE hashbit Index secondary_index_last on REGISTRATION(last);";
-            stmt.executeUpdate(sql);
-            System.out.println("Created index in given database...");
-
-            sql = "Insert into Registration(first, last, age) values ('first15', 'last15', 23);";
-            stmt.executeUpdate(sql);
-
-            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
-            stmt.executeUpdate(sql);
-
-            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
-            stmt.executeUpdate(sql);
-
-            sql = "Insert into Registration(first, last, age) values ('brr26', 'brrlast26', 23);";
-            stmt.executeUpdate(sql);
-
-            sql = "UPDATE Registration SET  first = 'update6', last = 'update6' WHERE ID = 1;";
-            stmt.executeUpdate(sql);
-
-            sql = "Delete from Registration where id = '3'";
-            stmt.executeUpdate(sql);
 
 //
 //
-//
-//
-//            //STEP 3: Execute a query
-//            System.out.println("Drop table in given database...");
-//            stmt = conn.createStatement();
-//            sql =  "DROP TABLE REGISTRATION IF EXISTS;";
-//            stmt.executeUpdate(sql);
-//
-//            stmt = conn.createStatement();
-//            sql =  "DROP TABLE REGISTRATION2 IF EXISTS;";
-//            stmt.executeUpdate(sql);
 //
 //            sql = "CREATE TABLE REGISTRATION (id bigint auto_increment, first VARCHAR(255),last VARCHAR(255), age INTEGER, PRIMARY KEY (id));";
 //            stmt.executeUpdate(sql);
 //            System.out.println("Created table in given database...");
 //
-//            sql = "CREATE TABLE REGISTRATION2 (id bigint auto_increment, PRIMARY KEY (id));";
+//            String sql = "CREATE HASHBIT Index BUCKETS 4 secondary_index_last on demo(fname);";
 //            stmt.executeUpdate(sql);
-//            System.out.println("Created table in given database...");
+//            System.out.println("Created index in given database...");
 //
-//            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
+//            String sql = "Insert into demo(fname, lname) values ('first15', 'last15');";
 //            stmt.executeUpdate(sql);
 //
 //            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
 //            stmt.executeUpdate(sql);
 //
-//
-//            sql = "Insert into Registration(first, last, age) values ('first37', 'last37', 23);";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "Insert into Registration(first, last, age) values ('first48', 'last48', 23);";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
-//            stmt.executeUpdate(sql);
-//
 //            sql = "Insert into Registration(first, last, age) values ('first26', 'last26', 23);";
 //            stmt.executeUpdate(sql);
 //
-//
-//            sql = "CREATE hashbit Index BUCKETS 8 secondary_index on REGISTRATION(first);";
-//            stmt.executeUpdate(sql);
-//            System.out.println("Created index in given database...");
-//
-//            sql = "CREATE hashbit Index secondary_index_last on REGISTRATION(last);";
-//            stmt.executeUpdate(sql);
-//            System.out.println("Created index in given database...");
-//
-//            System.out.println("?????????????????????????????  This is Part 02 ????????????????????????????????????????????????????????");
-//
-//            sql = "Insert into Registration(first, last, age) values ('first37', 'last37', 23);";
+//            sql = "Insert into Registration(first, last, age) values ('brr26', 'brrlast26', 23);";
 //            stmt.executeUpdate(sql);
 //
-//
-//            sql = "Insert into Registration(first, last, age) values ('first48', 'last48', 23);";
+//            sql = "UPDATE Registration SET  first = 'update6', last = 'update6' WHERE ID = 1;";
 //            stmt.executeUpdate(sql);
 //
-//
-//            sql = "Insert into Registration(first, last, age) values ('first159', 'last159', 23);";
+//            sql = "Delete from Registration where id = '3'";
 //            stmt.executeUpdate(sql);
-//
-//            sql = "Insert into Registration() values ();";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "Insert into Registration() values ();";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "Insert into Registration(age) values (31);";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "Insert into Registration(first) values ('first13');";
-//            stmt.executeUpdate(sql);
-//
-////            null => 10, 11, 12
-//
-//            System.out.println("-----------------------------------------------------UPDATE---------------------------------------------------------------------");
-//
-//            sql = "UPDATE Registration SET  first = 'update6', last = 'update6' WHERE ID = 6;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "UPDATE Registration SET  first = 'update8', last = 'update8' WHERE ID = 8;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "UPDATE Registration SET  first = 'update1', last = 'update1' WHERE ID = 1;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "UPDATE Registration SET  first = 'update3', last = 'update3' WHERE ID = 3;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "UPDATE Registration SET  first = 'first13', last = 'last13' WHERE ID = 10;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "UPDATE Registration SET  first = NULL, last = NULL WHERE ID = 11;";
-//            stmt.executeUpdate(sql);
-//
-//            System.out.println("----------------------------------------------------DELETE----------------------------------------------------------------------");
-////
-////
-//            sql = "DELETE FROM Registration WHERE id = 3;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "DELETE FROM Registration WHERE id = 10;";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "DELETE FROM Registration WHERE id = 1;";
-//            stmt.executeUpdate(sql);
-////
-//            System.out.println("-----------------------------------------------------------SELECT-------------------------------------------------------");
 
-            // STEP 4: Clean-up environment
-            stmt.close();
-            conn.close();
+            closeConnections();
         } catch(SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
@@ -191,39 +125,28 @@ public class Main {
             } //end finally try
         } //end try
     }
+
+    private static void executeSQL(String sql) throws SQLException {
+        log.debug("Executing SQL: {}", sql);
+        stmt.executeUpdate(sql);
+    }
+
+    private static void initStmt() throws SQLException {
+        stmt = conn.createStatement();
+    }
+
+    private static void closeConnections() throws SQLException {
+        // STEP 4: Clean-up environment
+        stmt.close();
+        conn.close();
+    }
+
+    private static void setUpDbConnection() throws ClassNotFoundException, SQLException {
+        // STEP 1: Register JDBC driver
+        Class.forName(JDBC_DRIVER);
+
+        //STEP 2: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+    }
 }
-
-//first26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//first37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//first48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 ]
-//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0 ]
-//first13 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
-//first159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
-
-//update 6 8 1 3
-
-//first26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//first37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
-//first48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 ]
-//first13 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
-//update8 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ]
-//update6 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ]
-//first159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 ]
-
-
-//last26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//last37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//last48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 ]
-//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 ]
-//last159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 ]
-
-//update 6 8 1 3
-
-//last26 :- [0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ]
-//last37 :- [0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//last48 :- [0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 ]
-//NULL :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0 , 0 ]
-//update3 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 ]
-//last159 :- [1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
-//update1 :- [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 ]
---
+++ src/main/java/SqlHelper.java
@@ -0,0 +1,55 @@
+import org.h2.table.Column;
+import java.util.List;
+
+public class SqlHelper {
+
+    public static String getCreateTableSql(String tableName, Column primaryKey, List<Column> otherColumns) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE ").append(tableName).append(" (");
+        sb.append(primaryKey.getName()).append(" ").append(primaryKey.getType()).append(" AUTO_INCREMENT").append(" PRIMARY KEY").append(",");
+        for (Column column : otherColumns) {
+            sb.append(column.getName()).append(" ").append(column.getType()).append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(")");
+        return sb.toString();
+    }
+
+    public static String getDropTableSql(String tableName) {
+        return "DROP TABLE IF EXISTS " + tableName;
+    }
+
+    public static String getCreateHashIndexSql(String indexName, String tableName, String columnName) {
+        return "CREATE HASHBIT INDEX " + indexName + " on " + tableName + "(" + columnName + ")";
+    }
+
+    public static String getCreateHashIndexSql(String indexName, String tableName, String columnName, int bucketSize) {
+        return "CREATE HASHBIT INDEX BUCKETS " + bucketSize + " " + indexName +   " on " + tableName + "(" + columnName + ")";
+    }
+
+    public static String getInsertSql(String tableName, List<Column> columns, List<String> values) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO ").append(tableName).append(" (");
+        for (Column column : columns) {
+            sb.append(column.getName()).append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(") VALUES (");
+        for (String value : values) {
+            sb.append(wrapWithQuote(escape(value))).append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(")");
+        return sb.toString();
+    }
+
+    private static String escape(String str) {
+        return str.replace("'", "''");
+    }
+
+    private static String wrapWithQuote(String str) {
+        return "'" + str + "'";
+    }
+
+
+}
